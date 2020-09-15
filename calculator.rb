2781bca6fdb92f252a64407b237ea0d3e9ae84a8
Matthew Sallin <msallin@gmail.com>
2020-09-15 04:57:53 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number = 1
+second_number = 2
+sum = first_number + second_number
+#print sum
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
